0304d161b6fa3deb528ce3b36b2c564f56b22e42
Sudharshan Venkataraman <sudharshan24k@gmail.com>
2026-05-13 21:52:16 +0530

    Chore: Trigger deployments for both apps

--- public-website/src/main.jsx
+++ public-website/src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Deploy trigger: 2026-05-13T21:52:00
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
